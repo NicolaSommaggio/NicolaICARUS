77fa7f101c4ef50f26e9f118f43da967b670117b
NicolaSommaggio <sommaggio.nicola@gmail.com>
2026-07-19 22:34:04 -0500

    fix macro read tree selection output

--- PID/selection1muNp_PIDfinale/CHECK_SELECTION.C
+++ PID/selection1muNp_PIDfinale/CHECK_SELECTION.C
@@ -7,12 +7,13 @@ void CHECK_SELECTION()
 
     TTree * intree = (TTree*)infile->Get("tree");
 
-    _slice *slc;
+    _slice *slc = nullptr;
 
     intree -> SetBranchAddress("slice",&slc);
 
     TH1D *h_mu_pro_angle = new TH1D("h_mu_pro_angle","",100,-1,1);
     TH1D *h_mu_pro_angle_cut = new TH1D("h_mu_pro_angle_cut","",100,-1,1);
+    TH1D *h_p_as_pro_ris = new TH1D("h_p_as_pro_ris","",100,0,1);
 
     for(int i = 0; i < intree -> GetEntries(); i ++)
     {
@@ -21,10 +22,17 @@ void CHECK_SELECTION()
         h_mu_pro_angle -> Fill(slc->_mu_pro_angle);
 
         if(slc->_mu_pro_angle > -0.85){h_mu_pro_angle_cut -> Fill(slc->_mu_pro_angle);}
+
+        for(const auto &pro : slc -> _protons)
+        {
+            h_p_as_pro_ris -> Fill();
+        }
         
     }
 
+    TCanvas * c1 = new TCanvas("c1");
     h_mu_pro_angle -> Draw();
+    TCanvas * c2 = new TCanvas("c2");
     h_mu_pro_angle_cut -> Draw();
 
 }
