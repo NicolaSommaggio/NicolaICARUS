4f53a65832214236d71799d1fa1b5a1527a5b845
NicolaSommaggio <sommaggio.nicola@gmail.com>
2026-08-07 01:30:40 -0500

    update plotting (recoE spectrum) maple gump studies

--- MAPLE/selection_icarus/make_tree_eff_sel.C
+++ MAPLE/selection_icarus/make_tree_eff_sel.C
@@ -111,7 +111,7 @@ void make_tree_eff_sel()
   const std::string fdata_maple_gump_RUN4_concat_offbeam = "/pnfs/icarus/persistent/users/cfarnese/for_Nicola/offbeam_4.flat.caf.root";
 
 
- SpectrumLoader data(fdata_maple_gump_RUN4_concat);
+ SpectrumLoader data(fdata_maple_gump_RUN4_concat_offbeam);
 
 
     const SpillMultiVar True_Enu = kTrue_Enu;
@@ -315,7 +315,7 @@ void make_tree_eff_sel()
 //  TFile fout("/exp/sbnd/data/users/marterop/sel_dirt/selected_icarus_standard_eff_sel_MC_tmatch05_dirt_nocrt.root", "RECREATE");
   //TFile fout("/exp/sbnd/data/users/marterop/sel_dirt/selected_icarus_standard_eff_sel_MC_tmatch05_nocrt_nolight_MC_vars_dirt.root", "RECREATE");
   //TFile fout("selected_icarus_standard_eff_sel_MC_tmatch05_nocrt_nolight_MC_vars_offbeam_nuscore.root", "RECREATE");
-  TFile fout("SBRUCE_TREE_MAPLE_NICOLA_RUN4_MU_L_50_PRO_KE_50_CHI2_VAR_TRACKSCORE_VAR_VISIBLE_VAR_CONCAT.root", "RECREATE");
+  TFile fout("SBRUCE_TREE_MAPLE_NICOLA_RUN4_MU_L_50_PRO_KE_50_CHI2_VAR_TRACKSCORE_VAR_VISIBLE_VAR_CONCAT_OFFBEAM.root", "RECREATE");
   //TFile fout("SBRUCE_TREE_MAPLE_NICOLA_RUN4_MU_L_40_PRO_KE_40_CHI2_VAR_TRACKSCORE_VAR_VISIBLE_VAR_CONCAT.root", "RECREATE");
   //TFile fout("SBRUCE_TREE_MAPLE_NICOLA_RUN4_MU_L_40_PRO_KE_40_CHI2_VAR_TRACKSCORE_VAR_CONCAT.root", "RECREATE");
   //TFile fout("SBRUCE_TREE_MAPLE_NICOLA_RUN4_MU_L_40_PRO_KE_40_CHI2_VAR_CONCAT.root", "RECREATE");
